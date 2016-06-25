b650c209ade4a34b3915925f5090dbcc89f5b98e
Vincent Foley <vfoley@gmail.com>
2016-06-25 16:22:15 -0400

    Re-haul user interface
    - The old scheme of argv[1] being the number of dice, argv[2] the number
      of faces, and argv[3] the (optional) extra is gone.
    - Can accept multiple (0 or more) rolls in D&D notation (e.g., 1d6,
      3d8-4, 4d4+1)
    - Uses regex to parse a roll spec.
    - If an argument is not a valid roll spec, print an error for that roll,
      keep processing the next rolls.

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,75 @@
+extern crate regex;
+
 use std::env::args;
-use std::process::exit;
 
-/*
-TODO(vfoley):
-- Implement D&D-like dice syntax (c.f.: GRAMMAR)
-- Display: minimum, maximum, expected value
-- Flags: --min, --max, --ev, --all [default]
-
-GRAMMAR
-=======
-non_zero_digit ::= '1' | ... | '9'
-digit          ::= '0' | non_zero_digit
-int            ::= non_zero_digit { digit }
-roll           ::= int 'd' int [ modifier ]
-modifier       ::= '+' int
-                 | '-' int
-*/
+use regex::Regex;
 
 
-fn usage() -> ! {
-    println!("Usage: ev NUM_DICE NUM_FACES");
-    exit(1);
+/// A roll consists of:
+/// - A number of dice (positive integer)
+/// - A number of faces (positive integer)
+/// - An extra (e.g., +3 or -4)
+struct Roll {
+    num_dice: f32,
+    num_faces: f32,
+    extra: f32,
 }
 
-fn ev(num_dice: u32, num_faces: u32) -> f64 {
-    let nd = num_dice as f64;
-    let nf = num_faces as f64;
-    let single_die_ev = (nf + 1.0) / 2.0;
-    nd * single_die_ev
+impl Roll {
+    /// Compute the expected value: expected value of one die
+    /// multiplied by the number of dice, then add the extra.
+    fn ev(&self) -> f32 {
+        // \sum_{i=1}^{n} = n(n+1) / 2
+        // therefore
+        // 1/n * \sum_{i=1}^{n} = (n+1) / 2
+        let single_die_ev = (self.num_faces + 1.0) / 2.0;
+        self.num_dice * single_die_ev + self.extra
+    }
+
+    /// Compute the minimum value.
+    fn min(&self) -> f32 {
+        self.num_dice + self.extra
+    }
+
+    /// Compute the maximum value.
+    fn max(&self) -> f32 {
+        self.num_dice * self.num_faces + self.extra
+    }
+
+    fn print(&self) {
+        println!("\tmin: {}", self.min());
+        println!("\tmax: {}", self.max());
+        println!("\tev : {}", self.ev());
+    }
 }
 
 fn main() {
     let argv: Vec<String> = args().collect();
-    if argv.len() != 3 {
-        usage();
-    }
+    let roll_re = Regex::new(r"(?x)
+      ([1-9][0-9]*)             # Number of dice
+      d                         # The literal 'd'
+      ([1-9][0-9]*)             # Number of faces
+      ([+-][1-9][0-9]*)?        # Optional extra
+    ").unwrap();
 
-    match (argv[1].parse::<u32>(), argv[2].parse::<u32>()) {
-        (Ok(nd), Ok(nf)) => println!("{}", ev(nd, nf)),
-        (_, _) => usage(),
+    for arg in &argv[1..] {
+        match roll_re.captures(arg) {
+            Some(cap) => {
+                let nd = cap.at(1).unwrap().parse::<f32>().unwrap();
+                let nf = cap.at(2).unwrap().parse::<f32>().unwrap();
+                let ex = cap.at(3).unwrap_or("0").parse::<f32>().unwrap();
+                let roll = Roll {
+                    num_dice: nd,
+                    num_faces: nf,
+                    extra: ex,
+                };
+                println!("{}", *arg);
+                roll.print();
+            }
+
+            None => {
+                println!("ev: invalid format: {}", arg);
+            }
+        }
     }
 }
